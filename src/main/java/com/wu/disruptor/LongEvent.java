f73bce5dd5a426b2860611da8cbf57c42398f7fe
吴小青 <wuxiaoqing_44272@wdai.com>
2018-08-26 17:15:27 +0800

    disruptor

---
+++ src/main/java/com/wu/disruptor/LongEvent.java
@@ -0,0 +1,16 @@
+package com.wu.disruptor;
+
+/**
+ * 定义一个事件
+ */
+public class LongEvent {
+    private Long longValue ;
+
+    public Long getLongValue() {
+        return longValue;
+    }
+
+    public void setLongValue(Long longValue) {
+        this.longValue = longValue;
+    }
+}
