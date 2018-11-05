10f8b454d4de87fd46fc1968e81b78d45f35417a
wxq8783 <wxq_8783@126.com>
2018-11-05 23:05:55 +0800

    algorithm

---
+++ boot-algorithm/src/main/java/com/wu/list/reverse/Node.java
@@ -0,0 +1,28 @@
+package com.wu.list.reverse;
+
+public class Node {
+
+    private String value;
+    private Node next;
+
+    public Node(String value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
